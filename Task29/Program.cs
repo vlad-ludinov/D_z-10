efc7651c02b57434b1cac2fc7df0e1a1a553433c
vlad <vladislavludinov3006@gmail.com>
2023-01-18 20:10:24 +0300

    method createArray added in Task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+
+
+
+
+
+int [] createArray()
+{
+    int [] array = new int [8];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random(min,max);
+    }
+}
