496498f951a7892b29b11046b51d3af4d71b8bac
vlad <vladislavludinov3006@gmail.com>
2023-01-18 18:54:50 +0300

    method Pow added

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+int Pow(int number1, int number2)
+{
+    int result = number1;
+    for(int i = 0; i < number2-1; i++)
+    {
+        result*=number1;
+    }
+    return result;
+}
